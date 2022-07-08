022964cc22771fe2504a56b2b1c8959c61f502d5
xcloudxgamex <ravigameboy@gmail.com>
2022-07-08 22:33:34 +0530

    Track_3_Assignment_2
    Creation of Tank
    - Create a Prefab of the tank.
    - Spawn the tank prefab in Scene.

---
+++ Assets/Scripts/tank_spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tank_spawner : MonoBehaviour
+{
+    public GameObject tankPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
